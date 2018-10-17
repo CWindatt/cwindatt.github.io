2f758a8caefa202e4ce85a0fa8c011a1ee0520cb
Curtis Windatt <curtis.windatt.public@gmail.com>
2018-10-16 21:37:29 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+<TITLE>LaBelle Taekwondo</TITLE>
+<meta name="description" content="LaBelle Taekwondo is a fun, affordable and friendly place to learn the martial art of Taekwondo.  Master Brian LaBelle, along with his well trained assistant instructors, will teach you the technique and tradition of the Korean art.  We are recognized members of the World Taekwondo Federation (and WTF Canada).  Our classes are open to all ages and skill levels.  We emphasize the self-defense training, tradition and sport of Taekwondo.">
+<meta name="keywords" content="taekwondo winnipeg manitoba martial arts self defense canada brian labelle lisa curtis windatt grant bartel grandmaster am lee punch kick master korea korean tradition sport fitness">
+</head>
+
+<BODY bgcolor="#FFFFFF" text="black" link="#3665b4" alink="#e81b16" vlink="#e81b16">
+
+<TABLE WIDTH="100%" border=0>
+
+<!-- TKD LOGO  -->
+
+	<TR>
+		<TD ALIGN="center" WIDTH="150" ROWSPAN="2">
+			<img src="images/LaBelleLogo.png" align="top" width="150" alt="LaBelle Taekwondo Logo"><br>
+		</TD>
+
+<!-- TKD TITLE -->
+
+		<TD ALIGN="center" COLSPAN="2">
+			<img src="images/LaBelleTKD.png" align="top" alt="LaBelle Taekwondo">
+		</TD>
+	</TR>
+
+<!-- PAGE TITLE -->
+
+	<TR>
+		<TD WIDTH="8">
+
+		</TD>
+		<TD>
+			<font size=6 color="#e81b16"><center><b>Self-Defense, Tradition, Sport</b></center></font>
+		</TD>
+	</TR>
+
+<!-- LINKS TO OTHER PAGES -->
+
+<?php include("includemenu.php"); ?>
+
+<!-- TEXT BODY -->
+
+		<TD WIDTH="8"></TD>
+
+		<TD VALIGN="top">
+<br>
+<center><img src="images/Class2.png" border=0 padding=5></center>
+
+<p>Welcome to LaBelle Taekwondo!</p>
+
+<p>We offer a friendly and affordable martial art class, open to all ages and skill levels.  Taekwondo is an excellent way to improve your fitness and spirit.  Learn the traditions of the Korean martial art of Taekwondo, while gaining effective self-defense techniques and getting involved in an international Olympic sport.</p>
+
+<p>Come try out the class for free!  Call us at (204) 223-8693 or email us at <a href="mailto:info@labelletkd.com">info@labelletkd.com</a></b></p>
+
+<p align="center">
+<iframe width="560" height="315" src="http://www.youtube.com/embed/fNr-t21neTk" frameborder="0" allowfullscreen></iframe>
+</p>
+
+		</TD>
+	</TR>
+
+<!-- COPYRIGHT -->
+
+<?php include("includecopyright.php"); ?>
+
+
+</TABLE>
+</BODY></HTML>
+
